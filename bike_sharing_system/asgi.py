a1762ca03e1bf62204b66f388661d9256c911921
miriamwojcik <wk.miriam@gmail.com>
2021-07-25 04:26:56 +0100

    Apps not loaded yet fix

--- bike_sharing_system/asgi.py
+++ bike_sharing_system/asgi.py
@@ -9,9 +9,13 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
 
 import os
 
-from django.core.asgi import get_asgi_application
-
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bike_sharing_system.settings')
+
+import django
+
+django.setup()
+
+from django.core.asgi import get_asgi_application
 
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter
